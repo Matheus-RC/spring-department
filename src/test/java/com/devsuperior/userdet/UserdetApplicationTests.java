0d8273067ac757f6b2cff7f6d574d6923a1ff2f8
Matheus-RC <matheus.rc33@gmail.com>
2023-08-01 22:18:53 -0300

    primeiro commit

---
+++ src/test/java/com/devsuperior/userdet/UserdetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.userdet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
